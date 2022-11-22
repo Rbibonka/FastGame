d10a83e690a133c5674811db438d97dc1fba0818
Rbibonka <mamakao.oop@gmail.com>
2022-11-22 22:23:48 +0700

    FirstCommit

---
+++ FastGame/Assets/Scripts/Abstract classes/CharacterMove.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public abstract class CharacterMove : MonoBehaviour
+{
+    protected abstract void Moving();
+
+    protected abstract void Flip();
+
+}
---
+++ FastGame/Assets/Scripts/Player/PlayerMove.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerMove : CharacterMove
+{
+    [Range(0, 20)]
+    [SerializeField]
+    private float characterSpeed = 5f;
+
+    private Rigidbody2D characterBody;
+
+    private Vector2 moveInput;
+
+    private bool LookAtRight = true;
+
+    private void Start()
+    {
+        characterBody = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        if (!LookAtRight && moveInput.x > 0)
+        {
+            Flip();
+        }else if (LookAtRight && moveInput.x < 0)
+        {
+            Flip();
+        }
+    }
+
+     void FixedUpdate()
+    {
+        moveInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        
+        Moving();
+    }
+
+    protected override void Flip()
+    {
+        LookAtRight = !LookAtRight;
+
+        Vector2 scaler = transform.localScale;
+
+        scaler.x *= -1;
+
+        transform.localScale = scaler;
+    }
+
+    protected override void Moving()
+    {
+        characterBody.velocity = new Vector2(moveInput.x * characterSpeed, moveInput.y * characterSpeed);
+    }
+}
